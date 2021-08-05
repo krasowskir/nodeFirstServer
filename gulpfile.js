01bc796798d441431d9d395e5f3e58a6358cb464
Richard Krasowski <richard.krasowski@mytoys.de>
2021-08-05 17:02:55 +0200

    version 2 - add proper build tassk with babel and copying files from src directory - create index.html in build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,29 @@
-var gulp = require('gulp')
+var { src, dest } = require('gulp')
+var babel = require('gulp-babel')
 
 function defaultTask(cb){
     console.log("Ich lerne react");
     cb();
 }
+function clean(cb){
+    //clean stuff
+    cb();
+}
+
+function build(){
+    return src('./src/*.js')
+        .pipe(babel({
+            presets: [
+              [
+                "@babel/env",
+                {
+                  modules: false
+                }
+              ]
+            ]
+          })
+        )
+        .pipe(dest('./build/'));
+}
 exports.default = defaultTask
+exports.build = build
